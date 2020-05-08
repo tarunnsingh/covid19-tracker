b3bd84dd8a0178a755b8467503b425be204c17ed
Tarun Singh <31896659+thetseffect@users.noreply.github.com>
2020-05-09 00:08:00 +0530

    Started Twitter API Connection

---
+++ src/api/twitter.js
@@ -0,0 +1,9 @@
+const Twit = require('twit');
+const notifier = require('node-notifier');
+const open = require('open')
+const franc = require('franc')
+
+const apikey = ''
+const apiSecretKey=''
+const accessToken=''
+const accessTokenSecret =''
