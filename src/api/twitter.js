e21cb95a492795f8f3c05669c055f48f14606816
thetseffect <thetseffect@gmail.com>
2020-05-09 01:42:36 +0530

    Updating Twitter.js

--- src/api/twitter.js
+++ src/api/twitter.js
@@ -1,9 +1,27 @@
-const Twit = require('twit');
-const notifier = require('node-notifier');
-const open = require('open')
-const franc = require('franc')
+const Twit = require("twit");
+const notifier = require("node-notifier");
+const open = require("open");
+const franc = require("franc");
+const creds = require("../credentials")();
 
-const apikey = ''
-const apiSecretKey=''
-const accessToken=''
-const accessTokenSecret =''
+let T = new Twit({
+  consumer_key: creds.apikey,
+  consumer_secret: creds.apiSecretKey,
+  access_token: creds.accessToken,
+  access_token_secret: creds.accessTokenSecret,
+});
+
+// T.get("search/tweets", { q: "#coronavirus", count: 2000 }, (err, data, response) => {
+//   const tweets = data.statuses
+//     .map((tweet) => `${tweet.text}`)
+//     .filter((tweet) => tweet = franc(tweet) === "eng" ? tweet : null)
+//     .filter((tweet) => tweet.toLowerCase().includes('trump'))
+//   console.log(tweets);
+// });
+
+
+const stream = T.stream('statuses/filter', { track: '#covid'})
+stream.on('tweet', (tweet) => {
+    console.log(tweet.text);
+    console.log('--------')
+})
