ac9a9ff4d0971ef58788ed4aa7ce648bdc2ae407
thetseffect <thetseffect@gmail.com>
2020-05-11 00:57:34 +0530

    added chips

--- src/api/twitter.js
+++ src/api/twitter.js
@@ -8,31 +8,30 @@ let T = new Twit({
   consumer_key: creds.apikey,
   consumer_secret: creds.apiSecretKey,
   access_token: creds.accessToken,
-  access_token_secret: creds.accessTokenSecret
+  access_token_secret: creds.accessTokenSecret,
 });
 
-const searchTweets = () => {
+const searchTweets = (queryString, sortOnString) => {
   try {
-    T.get(
-      "search/tweets",
-      { q: "#coronavirus", count: 2000 },
-      (err, data, response) => {
-        const tweets = data.statuses
-          .map(tweet => `${tweet.text}`)
-          .filter(tweet => (tweet = franc(tweet) === "eng" ? tweet : null))
-          .filter(tweet => tweet.toLowerCase().includes("trump"));
-        console.log(tweets);
-        return tweets;
-      }
-    );
+    T.get("search/tweets", { q: queryString, count: 2000 }, (err, data, response) => {
+      const tweets = data.statuses
+        .map((tweet) => `${tweet.text}`)
+        .filter((tweet) => (tweet = franc(tweet) === "eng" ? tweet : null))
+        .filter((tweet) => tweet.toLowerCase().includes(sortOnString));
+      console.log(tweets);
+      return tweets;
+    });
   } catch (error) {
     console.log(error);
   }
 };
 
-// Branched here
+//Branched here
+
 // const stream = T.stream('statuses/filter', { track: '#covid'})
 // stream.on('tweet', (tweet) => {
 //     console.log(tweet.text);
 //     console.log('--------')
 // })
+
+
--- src/components/TweetCard/TweetCard.jsx
+++ src/components/TweetCard/TweetCard.jsx
@@ -16,36 +16,41 @@ import ShareIcon from "@material-ui/icons/Share";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-const useStyles = makeStyles(theme => ({
+import styles from "./TweetCard.module.scss";
+
+const useStyles = makeStyles((theme) => ({
   root: {
-    width: "70%"
+    width: "70%",
   },
   media: {
     height: 0,
-    paddingTop: "56.25%" // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   expand: {
     transform: "rotate(0deg)",
     marginLeft: "auto",
     transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest
-    })
+      duration: theme.transitions.duration.shortest,
+    }),
   },
   expandOpen: {
-    transform: "rotate(180deg)"
+    transform: "rotate(180deg)",
   },
   avatar: {
-    backgroundImage:
-      "https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter_colored_svg-512.png"
-  }
+    backgroundImage: "https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter_colored_svg-512.png",
+  },
 }));
 
 export default function RecipeReviewCard() {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+  const [tags, setTags] = React.useState(["Covid-19", "Vaccine"]);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const addTags = (event) => {
+    if (event.target.onKeyUp == "Enter") {
+      setTags([...tags, event.target.value]);
+      event.target.value = "";
+    }
   };
 
   return (
@@ -60,17 +65,22 @@ export default function RecipeReviewCard() {
         title="Latest tweets with #coronavirus"
         subheader="Tweets being fetched in real time"
       />
-      <CardMedia
-        className={classes.media}
-        image="../../images/virus.svg"
-        title="Covid-19 Pandemic"
-      />
+      <CardMedia className={classes.media} image="../../images/virus.svg" title="Covid-19 Pandemic" />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
+          {}
         </Typography>
+        <div className={styles.tagsInput}>
+          <ul>
+            {tags.map((tag, index) => {
+              <li key={index}>
+                <span>{tag}</span>
+                <i className={styles.tagCloseIcon}>close</i>
+              </li>;
+            })}
+          </ul>
+          <input placeholder="Enter covid related tags here..." type="text" onKeyUp={addTags} />
+        </div>
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="more">
@@ -79,48 +89,7 @@ export default function RecipeReviewCard() {
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
